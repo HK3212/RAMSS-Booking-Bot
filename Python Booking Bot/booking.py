73c1f4facbedce7a9940885cf68930af992d08f7
Hamza Khan <hamza.a.khan@ryerson.ca>
2017-09-05 21:02:08 -0400

    Changed code to meet Python conventions.

--- Python Booking Bot/booking.py
+++ Python Booking Bot/booking.py
@@ -1,48 +1,49 @@
-import webbrowser
-import pyautogui
 from datetime import timedelta
 from datetime import datetime
+import webbrowser
+import pyautogui
 
 #Finds the difference between two dates.
 
-def days_between(d1, d2):
-    d1 = datetime.strptime(d1, "%Y-%m-%d")
-    d2 = datetime.strptime(d2, "%Y-%m-%d")
-    return abs((d2 - d1).days)
+def days_between(day1, day2):
+    """Determines the number of days between two dates."""
+    day1 = datetime.strptime(day1, "%Y-%m-%d")
+    day2 = datetime.strptime(day2, "%Y-%m-%d")
+    return abs((day2 - day1).days)
 
 logins = {
-    'User1': 'Pass1'
 }
 
-fridayLogins = {
-}
+BOOKING_URL = "http://apps.library.ryerson.ca/room_booking/booking/book_room?slot="
+
+DAY_OF_WEEK = datetime.today().weekday()
 
 #Amount slot increases by every 2 hours.
 
-twoHourInc = 7200
+TWO_HOUR_INCREASE = 7200
 
 #Amount slot increases by per day.
 
-dailyInc = 86400
+DAILY_INCREASE = 86400
 
 #Slot of first booking day.
 
-710slot = 0
-firstDay = "2017-05-17"
+slot710 = 0
+FIRST_DAY = "2017-05-17"
 
 #Day of booking (3 weeks from current date).
 
-bookingDay = (datetime.today() + timedelta(days=21)).strftime("%Y-%m-%d")
+BOOKING_DAY = (datetime.today() + timedelta(days=21)).strftime("%Y-%m-%d")
 
 #Add daily increase to slot days_between times
 
-for i in range(0, days_between(firstDay, bookingDay)):
-    slot = slot + dailyInc
+for i in range(0, days_between(FIRST_DAY, BOOKING_DAY)):
+    slot710 = slot710 + DAILY_INCREASE
 
-temp = 710slot
+temp = slot710
 pyautogui.PAUSE = 4
 for user in logins:
-    webbrowser.open("http://apps.library.ryerson.ca/room_booking/booking/book_room?slot=" + str(temp) + "&room_id=213")
+    webbrowser.open(BOOKING_URL + str(temp) + "&room_id=213")
     pyautogui.press('tab')
     pyautogui.PAUSE = 0.1
     pyautogui.typewrite(user)
@@ -51,13 +52,13 @@ for user in logins:
     pyautogui.press('tab')
     pyautogui.press('enter')
     pyautogui.PAUSE = 4
-    for i in range(0,14):
+    for i in range(0, 14):
         pyautogui.press('tab')
         pyautogui.PAUSE = 0.1
     pyautogui.press(['enter', 'down', 'down', 'down', 'enter', 'tab', 'tab', 'enter'])
     pyautogui.PAUSE = 3
-    for i in range(0,18):
+    for i in range(0, 18):
         pyautogui.press('tab')
         pyautogui.PAUSE = 0.1
     pyautogui.press('enter')
-    temp = temp + twoHourInc
+    temp = temp + TWO_HOUR_INCREASE
